c6639ca4b048e0ad3da0b2eb3c35afd2206c697e
Rogerio Vinhal Nunes <rogerio@vnunes.win>
2025-12-31 20:26:52 +0000

    added thinknixos and gave permissions for secrets

--- nixos/nixos-desktop/flake.nix
+++ nixos/nixos-desktop/flake.nix
@@ -14,68 +14,94 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      agenix,
-      home-manager,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations.amdesktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          userName = "rogervn";
-          hostName = "amdesktop";
-          inherit inputs;
-        };
-        modules = [
-          ./configuration.nix
-          ./home.nix
-          ../hosts/amdesktop/hardware-configuration.nix
-          ../modules/base.nix
-          ../modules/secrets.nix
-          ../modules/steam.nix
-          ../modules/window_manager.nix
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-          {
-            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
-          }
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+    agenix,
+    home-manager,
+    ...
+  } @ inputs: {
+    nixosConfigurations.amdesktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        userName = "rogervn";
+        hostName = "amdesktop";
+        inherit inputs;
       };
-      nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          userName = "rogervn";
-          hostName = "nixos-vm";
-          inherit inputs;
-        };
-        modules = [
-          ./configuration.nix
-          ./home.nix
-          ../hosts/nixos-vm/hardware-configuration.nix
-          ../modules/base.nix
-          ../modules/secrets.nix
-          ../modules/vm_guest.nix
-          ../modules/window_manager.nix
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-          {
-            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
-          }
-        ];
-      };
+      modules = [
+        ./configuration.nix
+        ./home.nix
+        ../hosts/amdesktop/hardware-configuration.nix
+        ../modules/base.nix
+        ../modules/secrets.nix
+        ../modules/steam.nix
+        ../modules/window_manager.nix
+        agenix.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {inherit inputs;};
+        }
+        {
+          environment.systemPackages = [agenix.packages.x86_64-linux.default];
+        }
+      ];
     };
+
+    nixosConfigurations.thinknixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        userName = "rogervn";
+        hostName = "thinknixos";
+        inherit inputs;
+      };
+      modules = [
+        ./configuration.nix
+        ./home.nix
+        ../hosts/amdesktop/hardware-configuration.nix
+        ../modules/base.nix
+        ../modules/secrets.nix
+        ../modules/window_manager.nix
+        agenix.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {inherit inputs;};
+        }
+        {
+          environment.systemPackages = [agenix.packages.x86_64-linux.default];
+        }
+      ];
+    };
+
+    nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        userName = "rogervn";
+        hostName = "nixos-vm";
+        inherit inputs;
+      };
+      modules = [
+        ./configuration.nix
+        ./home.nix
+        ../hosts/nixos-vm/hardware-configuration.nix
+        ../modules/base.nix
+        ../modules/secrets.nix
+        ../modules/vm_guest.nix
+        ../modules/window_manager.nix
+        agenix.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {inherit inputs;};
+        }
+        {
+          environment.systemPackages = [agenix.packages.x86_64-linux.default];
+        }
+      ];
+    };
+  };
 }
