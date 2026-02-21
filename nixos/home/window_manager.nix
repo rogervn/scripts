9102f0f23cf0972f4e0ec17d008d1cf0e016b8ec
Rogerio <rogerio@vnunes.win>
2026-02-21 11:13:32 +0000

    added hyprland portal

--- nixos/home/window_manager.nix
+++ nixos/home/window_manager.nix
@@ -55,6 +55,19 @@
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [xdg-desktop-portal-hyprland xdg-desktop-portal-gtk];
+    config = {
+      hyprland = {
+        default = ["hyprland" "gtk"];
+        "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+        "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
+        "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
+      };
+    };
+  };
+
   gtk = {
     enable = true;
     theme = {
