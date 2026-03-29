9d4646d84289fb3c9adfb76e23f7d851b641d834
Rogerio Vinhal Nunes <rogerio@vnunes.win>
2026-03-29 19:55:47 +0100

    split home-manager hyprland into a shared hyprland nix file between nixos and non-nixos

--- nixos/home/window_manager.nix
+++ nixos/home/window_manager.nix
@@ -4,7 +4,10 @@
   pam_shim,
   ...
 }: {
-  imports = [pam_shim.homeModules.default];
+  imports = [
+    pam_shim.homeModules.default
+    ./hyprland.nix
+  ];
   pamShim.enable = true;
   nixpkgs.overlays = [
     (final: prev: {
@@ -39,64 +42,4 @@
     xdg-user-dirs
     bibata-cursors
   ];
-
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-  };
-
-  systemd.user.services.hyprpolkitagent = {
-    Unit = {
-      Description = "Hyprland Polkit Agent";
-      After = [
-        "graphical-session.target"
-        "dbus.service"
-      ];
-    };
-    Service = {
-      ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
-      Restart = "on-failure";
-    };
-    Install = {
-      WantedBy = ["graphical-session.target"];
-    };
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [xdg-desktop-portal-hyprland xdg-desktop-portal-gtk];
-    config = {
-      hyprland = {
-        default = ["hyprland" "gtk"];
-        "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
-        "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
-        "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
-      };
-    };
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Orchis-Dark";
-      package = pkgs.orchis-theme;
-    };
-
-    iconTheme = {
-      name = "Tela-blue-dark";
-      package = pkgs.tela-icon-theme;
-    };
-
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-  };
-
-  # Dconf Settings for Global Dark Mode (Libadwaita/Modern GTK4)
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
 }
--- nixos/modules/window_manager.nix
+++ nixos/modules/window_manager.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  userName,
-  ...
-}: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     bibata-cursors
     bitwarden-desktop
@@ -79,39 +75,6 @@
   services.power-profiles-daemon.enable = true;
 
   security.pam.services.login.enableGnomeKeyring = true;
-
-  home-manager.users.${userName} = {
-    home.pointerCursor = {
-      gtk.enable = true;
-      x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 24;
-    };
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Orchis-Dark";
-        package = pkgs.orchis-theme;
-      };
-
-      iconTheme = {
-        name = "Tela-blue-dark";
-        package = pkgs.tela-icon-theme;
-      };
-
-      gtk4.theme = null;
-      gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-    };
-
-    # Dconf Settings for Global Dark Mode (Libadwaita/Modern GTK4)
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-    };
-  };
 
   security.polkit.enable = true;
 
