1cc7a87809981d72167d34dc66f5b78a2d5489e4
Rogerio <rogerio@vnunes.win>
2026-04-25 16:55:31 +0100

    corrections to tailscale

--- nixos/modules/tailscale.nix
+++ nixos/modules/tailscale.nix
@@ -1,8 +1,9 @@
 {config, ...}: {
   services.tailscale = {
     enable = true;
+    port = 41641;
     authKeyFile = config.age.secrets.tailscale_auth_key.path;
-    useRoutingFeatures = "both"; # enables IP forwarding + loose rp_filter
+    useRoutingFeatures = "server";
     extraUpFlags = [
       "--accept-routes"
       "--advertise-exit-node"
@@ -10,4 +11,5 @@
       "--advertise-routes=10.0.0.0/24"
     ];
   };
+  networking.firewall.allowedUDPPorts = [41641];
 }
