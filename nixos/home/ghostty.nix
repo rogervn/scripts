6076c870c0e6eb3e123ab6fce6ba57d3642b49d2
Rogerio <rogerio@vnunes.win>
2026-07-28 10:01:18 +0100

    added @ as word delimiter for ghostty

--- nixos/home/ghostty.nix
+++ nixos/home/ghostty.nix
@@ -8,6 +8,8 @@
       title = "Ghostty";
       gtk-titlebar = false;
       theme = "noctalia";
+      # added '@' as a selection word char for user@host only select host
+      selection-word-chars = "\" \\t'\\\"│`|:;,()[]{}<>$@\"";
     };
   };
 }
